84e909046207e1c3f633939ead242c9c8252c770
Zeke Sikelianos <zeke@sikelianos.com>
2008-03-21 00:57:11 +0000

    Trying checkin of public directory alone
    git-svn-id: https://monoquery.googlecode.com/svn/trunk@49 cf6fe662-2141-0410-9b27-09d17ba8d87b

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+
+// If a command's publicity is set to private, its queries are necessarily private too
+// Toggle the unchchecking, disabling, and fading of the query publicity checkbox.
+function handle_publicity_checkboxes(input) {
+  var form = input.form;
+  var q = $('command_public_queries')
+  var l = $('command_public_queries_label')
+  if (input.checked) {
+    $('command_public_queries').checked = true;
+    q.disabled = false;
+    l.removeClassName("disabled");
+  } else {
+    q.checked = false;
+    q.disabled = true;
+    l.addClassName("disabled");
+  }  
+}
