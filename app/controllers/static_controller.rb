cecc309e3b9dec2d8e336bf656d925af5fe4822e
Gabriel Horner <gabriel.horner@gmail.com>
2008-06-10 08:06:45 +0000

    filter home page queries with more predictable # of queries
    git-svn-id: https://monoquery.googlecode.com/svn/trunk@132 cf6fe662-2141-0410-9b27-09d17ba8d87b

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,7 +3,9 @@ class StaticController < ApplicationController
   
   def home
     @queries = Query.public.non_empty.find(:all, :order => "queries.created_at DESC", 
-      :include => [{:user_command=>[:command, :user]}, :user], :limit=>30) 
+      :include => [{:user_command=>[:command, :user]}, :user], :limit=>45)
+    # keep out anonymous queries, too costly to do in db
+    @queries = @queries.select {|e| !e.user_id.nil? }.slice(0,30)
     @users = User.find_top_users 
     @commands = Command.find(:all, :order=>'commands.created_at DESC', :limit=>3, :include=>:user)
   end
