6445f58c5ae953995a3fb1b9aabe64dc99762023
Gabriel Horner <gabriel.horner@gmail.com>
2008-06-10 07:12:03 +0000

    home page tweaks including more command publicity
    git-svn-id: https://monoquery.googlecode.com/svn/trunk@130 cf6fe662-2141-0410-9b27-09d17ba8d87b

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,6 +5,7 @@ class StaticController < ApplicationController
     @queries = Query.public.non_empty.find(:all, :order => "queries.created_at DESC", 
       :include => [{:user_command=>[:command, :user]}, :user], :limit=>30) 
     @users = User.find_top_users 
+    @commands = Command.find(:all, :order=>'commands.created_at DESC', :limit=>3, :include=>:user)
   end
   
 end
